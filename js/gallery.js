08af053691c0a065d7fc3b7c13ff4fd453b4b7ff
lestgabo <lestgabo@gmail.com>
2018-07-30 02:11:58 +0000

    update image info booky way

--- js/gallery.js
+++ js/gallery.js
@@ -1,36 +1,32 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// 
+
 function activateGallery() {
   // either notation works, greater than sign or space
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage  = document.querySelector("#gallery-photo img");
   
-  
-  let infoTitle = document.querySelector("#gallery-info > h3");  
-  let infoDescription = document.querySelector("#gallery-info > p");
-  
-  // let currentSelected = document.querySelector("#gallery-thumbs > div");
-  // add a div.current right after gallery-thumbs
-  // galleryThumbsCurrent[2].classList.add("current")
-  // let galleryThumbsCurrent = document.querySelector("#gallery-thumbs > div");
-    
-
-  
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
-
 
       // Set clicked image as main image.
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
       mainImage.setAttribute("alt", thumbnail.alt);
-      
-      let newinfoTitle          = thumbnail.dataset.title;
-      let newInfoDescription    = thumbnail.dataset.description;
-      infoTitle.innerText       = newinfoTitle;      
-      infoDescription.innerText = newInfoDescription;
-      
+
       // Change which image is current
-      document.querySelector(".current").classList.remove("current");
-      thumbnail.parentNode.classList.add("current");
+      let currentClass = "current";
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+      
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");  
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+      
+      title.innerHTML       = thumbnail.dataset.title;      
+      description.innerHTML = thumbnail.dataset.description;
     });
   });  
   
